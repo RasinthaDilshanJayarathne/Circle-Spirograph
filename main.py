5cf3f9a0182f13f85ac78a7203ce9542eeb92127
Radintha Dilshan <rasinthadilshan9@gmail.com>
2021-11-14 12:42:15 +0530

    1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ turtle.bgcolor("black")
 turtle.pensize(1)
 turtle.speed(0)
 
-for i in range(6):
+for i in range(100):
     for colours in ["red","magenta","blue","cyan","green","yellow","white"]:
         turtle.color(colours)
         turtle.circle(100)
